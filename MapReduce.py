0b63fa1b89d17c895dafd5413c6f67b2fe1e3e53
padraigmitchell <padraig.mitchell@ucdconnect.ie>
2016-10-21 12:44:42 +0100

    Unique IP's

--- MapReduce.py
+++ MapReduce.py
@@ -2,11 +2,32 @@
 class Log:
     'Log file, its components and actions'
     def __init__(self, fname):
+        self.ips =[]
+        self.sumEntries(fname)
+        self.storeIPs(fname)
+
+
+    def sumEntries(self, fname):
         with open(fname) as file:
             self.totalConnections = sum(1 for _ in file)
+
+    def storeIPs(self, fname):
+        with open(fname) as file:
+            for line in file:
+                w = 0
+                for word in line.split():
+                    if w == 3:
+                        self.ips.append(word)
+                    w = w + 1
 
     def getTotalConnections(self):
         return self.totalConnections
 
+    def getAll_IPs (self):
+        return self.ips
+
+    def getAll_unique_ips (self):
+        self.uniqueIPS = list(set(self.ips))
+        return self.uniqueIPS
 
 
--- Test.py
+++ Test.py
@@ -1,3 +1,5 @@
 from MapReduce import Log
 l = Log('My log')
 print "Total number of connections = %d" %l.getTotalConnections()
+print "All the IP's = %r" %l.getAll_IPs()
+print "All the Unique IP's = %r" %l.getAll_unique_ips()
